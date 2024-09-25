d4bcd877c38685cc3f8411ef4caba529ff5efd57
Ori <ori100no@gmail.com>
2024-09-25 13:24:07 +0300

    added context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { ThemeProvider } from "@mui/material";
 import React from "react";
 import { basicTheme } from "./theme/basicTheme";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
+import HomePage from "./pages/HomePage";
+import { WeatherProvider } from "./context/WeatherContext";
+import WeatherPage from "./pages/WeatherPage";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={basicTheme}>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <WeatherProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/weather" element={<WeatherPage />} />
+        </Routes>
+      </WeatherProvider>
     </ThemeProvider>
   );
 };
