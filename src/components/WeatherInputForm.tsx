e105769928abeaace91cd7697532736e2790b6a1
Ori <ori100no@gmail.com>
2024-09-28 18:00:11 +0300

    removed redumdand box

--- src/components/WeatherInputForm.tsx
+++ src/components/WeatherInputForm.tsx
@@ -10,15 +10,13 @@ import InputMethodSelect from "./input-options/InputMethodSelect";
 const WeatherInputForm: React.FC = () => {
   const { inputMethod } = useWeatherFormContext();
   return (
-    <Box>
-      <Box className="flex flex-col md:flex-row md:items-start md:space-x-4 space-y-4 md:space-y-0 md:justify-between">
-        <InputMethodSelect />
-        <ForecastDurationSelect />
-        <Box className="flex flex-col md:flex-row md:justify-between md:space-x-4 space-y-4 md:space-y-0 w-full">
-          {inputMethod === "cityName" && <CityNameInput />}
-          {inputMethod === "coordinates" && <CoordinatesInput />}
-          {inputMethod === "currentLocation" && <CurrentLocationMessage />}
-        </Box>
+    <Box className="flex flex-col md:flex-row md:items-start md:space-x-4 space-y-4 md:space-y-0 md:justify-between">
+      <InputMethodSelect />
+      <ForecastDurationSelect />
+      <Box className="flex flex-col md:flex-row md:justify-between md:space-x-4 space-y-4 md:space-y-0 w-full">
+        {inputMethod === "cityName" && <CityNameInput />}
+        {inputMethod === "coordinates" && <CoordinatesInput />}
+        {inputMethod === "currentLocation" && <CurrentLocationMessage />}
       </Box>
     </Box>
   );
